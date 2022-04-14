e36bdf67037bc03605bd6a0509b462fd815315f8
ArtPoon <artpoon@gmail.com>
2022-04-14 15:07:43 -0400

    need to get download.sh to detect if user is running macOS, swap gtar for tar; updated fieldnames in mangolin

--- data_needed/download.sh
+++ data_needed/download.sh
@@ -1,9 +1,10 @@
 # download tarball from VirusSeq
-#wget -O virusseq.tar.gz https://singularity.virusseq-dataportal.ca/download/archive/all
+wget -O virusseq.tar.gz https://singularity.virusseq-dataportal.ca/download/archive/all
 # scan tarball for filenames
-filenames=$(gtar -ztf virusseq.tar.gz)
+filenames=$(tar -ztf virusseq.tar.gz)
 # stream FASTA data into xz-compressed file
-gtar -axf virusseq.tar.gz -O $filenames[0] | xz > virusseq.fasta.xz
+tar -axf virusseq.tar.gz -O $filenames[0] | xz > virusseq.fasta.xz
 # stream metadata into gz-compressed file
-gtar -axf virusseq.tar.gz -O $filenames[1] | gzip > virusseq.metadata.tsv.gz
-# ? rm virusseq.tar.gz
+tar -axf virusseq.tar.gz -O $filenames[1] | gzip > virusseq.metadata.tsv.gz
+# delete tarball
+rm virusseq.tar.gz
