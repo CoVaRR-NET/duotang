e85b2dbc248e2ec614d5e108c68515eb1f9d1874
Art Poon <artpoon@gmail.com>
2022-05-04 11:54:10 -0400

    revised legend order to match barplot, added docstring

--- scripts/subtype_plotter.R
+++ scripts/subtype_plotter.R
@@ -1,21 +1,33 @@
-plot.subvariants <- function(region='Canada', sublineage=c(name1),scaled=FALSE) {
+#' generate stacked barplot of a subset of lineages
+#' @param region:  char, can be used to select samples for a specific province
+#' @param sublineage:  char, vector of lineage names for subsetting
+#' @param scaled:  bool, display absolute or relative frequencies per week
+plot.subvariants <- function(region='Canada', sublineage=c(name1), 
+                             scaled=FALSE, col=NA) {
+  if (is.na(col)) {
+    col <- rainbow(length(sublineage))  # default colour palette
+  }
   VOCVOI1 <- data.frame(
     name=sublineage,
     pattern=sublineage,
-    color=rainbow(length(sublineage))
+    color=col
   )
   varmeta1 <- meta %>% filter(lineage %in% sublineage)
-  length(varmeta1$lineage)
+  #length(varmeta1$lineage)
+  
   variants1 <- sapply(VOCVOI1$pattern, function(p) 
     grepl(p, varmeta1$lineage, perl=T))
   varmeta1$pango.group <- varmeta1$lineage
   varmeta1$pango.group <- as.factor(varmeta1$pango.group)
+  
   varmeta1$week <- cut(varmeta1$sample.collection.date, 'week')
   varmeta1 <- varmeta1[as.Date(varmeta1$week) > as.Date('2021-11-30'), ]
   varmeta1$week <- as.factor(as.character(varmeta1$week))
+  
   pal <- VOCVOI1$color
   names(pal) <- VOCVOI1$name
   pal["other"] <- 'grey'  # named character vector
+  
   if (region=='Canada') {
     tab <- table(varmeta1$pango.group, varmeta1$week)  
   } else {
@@ -32,8 +44,8 @@ plot.subvariants <- function(region='Canada', sublineage=c(name1),scaled=FALSE) 
     barplot(tab2, col=pal2, 
             border=NA, las=2, cex.names=0.6, cex.axis=0.8, 
             ylab="Sequenced cases per week (%)") -> mp
-    legend(x=max(mp)+1, y=1, legend=levels(varmeta1$pango.group), 
-           bty='n', xpd=NA, cex=0.7, fill=pal2, 
+    legend(x=max(mp)+1, y=1, legend=rev(levels(varmeta1$pango.group)), 
+           bty='n', xpd=NA, cex=0.7, fill=rev(pal2), 
            x.intersp=0.5, y.intersp=1, border=NA)
   } 
   else {
