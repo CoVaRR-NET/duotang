71a3ec0e2776f451a40657dbff406c5be11d1295
raphaelraphael <raphael.poujol@gmail.com>
2023-01-16 19:43:48 -0500

    add Prince Edward Island

--- scripts/utils.R
+++ scripts/utils.R
@@ -3,7 +3,7 @@
 get.province.list <- function(region){
   # handle special values for prov
   if (region[1] == "East provinces (NL+NS+NB)") {
-    provlist <- c("Nova Scotia", "New Brunswick", "Newfoundland and Labrador")
+    provlist <- c("Nova Scotia", "New Brunswick", "Newfoundland and Labrador", "Prince Edward Island")
   } else if (region[1] == "Canada") {
     provlist <- unique(meta$province)
   } else {
