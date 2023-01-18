edfee87d973bf488a599bb5796ead7162631b281
raphaelraphael <raphael.poujol@gmail.com>
2023-01-18 15:18:38 -0500

    updates

--- duotangGISAID/subtype_plotter2.R
+++ duotangGISAID/subtype_plotter2.R
@@ -52,11 +52,17 @@ plot.subvariants <- function(region='Canada', sublineage,
     pal <- pal[match(levels(varmetaV1$pango_group), names(pal))]
   }
   
-
-  
+  if(nrow(varmeta1)>10){
+    varmeta1$week <- cut(varmeta1$sample_collection_date, 'week')
+    tab <- table(varmeta1$pango_group, varmeta1$week)
+    max.count <- max(apply(tab, 2, sum))
+  }
   if(nrow(varmetaV1)>10){
     varmetaV1$week <- cut(varmetaV1$sample_collection_date, 'week')
     tabV <- table(varmetaV1$pango_group, varmetaV1$week)
+    max.count=max(max.count,max(apply(tabV, 2, sum)))
+  }
+  if(nrow(varmetaV1)>10){
     if (scaled) {
         par(mar=c(5,5,1,5))
         tab2 <- apply(tabV, 2, function(x) x/sum(x))
@@ -77,20 +83,17 @@ plot.subvariants <- function(region='Canada', sublineage,
       y <- cases.wk[!is.na(idx)]
       y[is.na(y)] <- 0
       lab.y <- pretty(y)  # for drawing axis
-      max.count <- max(apply(tabV, 2, sum))
       y2 <- (y-min(y)) / (max(y)-min(y)) * max.count  # scale to variant counts
       at.y <- (lab.y-min(lab.y)) / (max(y)-min(y)) * max.count
       barplot(tabV, col=pal, 
               border=NA, las=2, cex.names=0.6, cex.axis=0.8, 
-              ylab="Sequenced cases per week",main = "VirusSeq") -> mp
+              ylab="Sequenced cases per week",main = "VirusSeq",ylim=range(pretty(c(0, max.count)))) -> mp
       lines(mp, y2, xpd=NA, col=rgb(0,0,0,0.5), lwd=3)
       axis(side=4, at=at.y, labels=format(lab.y, format = 'd'), hadj=0,
            las=1, cex.axis=0.7, col='grey50', col.ticks='grey50',
            col.axis='grey50')
     }}
   if(nrow(varmeta1)>10){
-    varmeta1$week <- cut(varmeta1$sample_collection_date, 'week')
-    tab <- table(varmeta1$pango_group, varmeta1$week)
     if (scaled) {
       par(mar=c(5,5,1,5))
       tab2 <- apply(tab, 2, function(x) x/sum(x))
@@ -111,12 +114,11 @@ plot.subvariants <- function(region='Canada', sublineage,
       y <- cases.wk[!is.na(idx)]
       y[is.na(y)] <- 0
       lab.y <- pretty(y)  # for drawing axis
-      max.count <- max(apply(tab, 2, sum))
       y2 <- (y-min(y)) / (max(y)-min(y)) * max.count  # scale to variant counts
       at.y <- (lab.y-min(lab.y)) / (max(y)-min(y)) * max.count
       barplot(tab, col=pal, 
               border=NA, las=2, cex.names=0.6, cex.axis=0.8, 
-              ylab="Sequenced cases per week",main = "GISAID") -> mp
+              ylab="Sequenced cases per week",main = "GISAID",ylim=range(pretty(c(0, max.count)))) -> mp
       lines(mp, y2, xpd=NA, col=rgb(0,0,0,0.5), lwd=3)
       axis(side=4, at=at.y, labels=format(lab.y, format = 'd'), hadj=0,
            las=1, cex.axis=0.7, col='grey50', col.ticks='grey50',
