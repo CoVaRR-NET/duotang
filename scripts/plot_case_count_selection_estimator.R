acbeb74f50866715dc8970a93ce817977e580e79
bfjia <bfjia@bfjia.net>
2023-02-23 13:39:42 -0800

    improve code documentation

--- scripts/plot_case_count_selection_estimator.R
+++ scripts/plot_case_count_selection_estimator.R
@@ -1,5 +1,9 @@
 library(splines)
 
+#' Reads in case count data from files in $data_dir for each province
+#' Return a named list for each province with key=shortname and value=df
+#' @param maxDate:  the maximum date to cutoff the data. Default=NA
+#' @param datadir:  the data directory where the agecount data is stored. Default='data_needed'
 parseCaseData<- function(maxDate = NA, datadir = "data_needed"){
   #maxDate = maxdate
   BC<-read.csv(gzfile(paste0(datadir, "/AgeCaseCountBC.csv.gz")), header=T)%>% 
@@ -50,6 +54,7 @@ parseCaseData<- function(maxDate = NA, datadir = "data_needed"){
     rename(Reported_Date = date) %>% #relabel date column.
     drop_na() #drop row if any col is NA
   
+  #this data havent been updated since Feb 2022
    #SK <- read.csv("data_needed/AgeCaseCountSK.csv", header=T) %>%
   #  filter(Region == "Total") %>% select (Date, Age.60.to.79, Age.80.) %>% #take the rows and columns we need
   #  mutate(Age60.79 =  Age.60.to.79 - lag( Age.60.to.79, default = 0)) %>% #get the diff to previos cell for age 60-79, this is the new per day
@@ -62,25 +67,29 @@ parseCaseData<- function(maxDate = NA, datadir = "data_needed"){
   return (list("BC" = BC, "ON" = ON, "QC"=QC, "AB"=AB, "Canada"=Canada))#, "SK"=SK))
 }
 
-#get smooth fit of casecounts
+#' get smooth fit of casecounts using knots
 getCaseCountSmoothFit <- function(countData, knots=5){
   return (smooth.spline(countData$Reported_Date, countData$n,nknots=knots))
 }
 
+#' get smooth fit of casecounts using lambda. THIS IS THE ONE CURRENTLY USED.
 getCaseCountSmoothFitWithLambda<-function(countData, lambda=0.001){
   return(smooth.spline(log10(countData$n),lambda=lambda))
 }
 
+#' get smooth fit of casecounts using spar. 
 getCaseCountSmoothFitWithSpar<-function(countData, spar=0.8){
   return(smooth.spline(log10(countData$n),spar=0.8))
 }
 
+#' The R implementation of the mathematica cublic spline algo. Seems incorrect tho.
 CubicSplSmooth <- function(countData, df=7) {
   #if (is.na(df)){df = ceiling(length(countData$n) / 20)}
   bs <- (lm(n ~ bs(Reported_Date, df = df), data = countData )$fit)
   return(bs)
 }
 
+#' An translation of the mathematica cublic spline algo by Sally
 CubicSplSmooth2 <- function(data, lambda=10^3) {
   data <- caseData$QC
   #data is a datafrome of 2 columns (date,n)
@@ -96,33 +105,21 @@ CubicSplSmooth2 <- function(data, lambda=10^3) {
   return(r)
 }
 
-CubicSplSmooth3 <- function(data, lambda=10^3) {
-  Knots <- c(rep(1, 3), seq(M), rep(M, 3))
-  X <- matrix(NA, M, M+2)
-  for (t in 1:M) {
-    for (n in 0:(M+1)) {
-      X[t, n+1] <- BSplineBasis(3, Knots, n, t)
-    }
-  }
-  Dsq <- diff(X, 2)
-  a <- lm.fit(X, data[,2], lambda*t(Dsq) %*% Dsq)
-  return(X %*% a$coefficients)
-}
-
-
-#plot the casecount by selection estimate. 
+#' plot the casecount by selection estimate. 
 plotCaseCountByDate2 <- function(countData, lineFits, population, region=NA, saveToFile=F){
   #countData <- caseCountData
   #lineFits <-rev(caseSelectionLines)
   #filename = "test"
   colors = list()
   rValues = list()
+  
+  #format each of the fit lines for different variants
   for (i in seq(1:length(lineFits))){
     fitData <- lineFits[[i]]$line
     colnames(fitData) <- c("Reported_Date", lineFits[[i]]$names, "type")
-    countData<- merge(countData, fitData %>% dplyr::select(-type), by = "Reported_Date", all = T)
-    colors[lineFits[[i]]$names] = lineFits[[i]]$color
-    rValues[lineFits[[i]]$names] = round(log(rev(countData[[lineFits[[i]]$names]])[1]/rev(countData[[lineFits[[i]]$names]])[2]) * 100,2)
+    countData<- merge(countData, fitData %>% dplyr::select(-type), by = "Reported_Date", all = T) #merge the X values
+    colors[lineFits[[i]]$names] = lineFits[[i]]$color #assign color to line
+    rValues[lineFits[[i]]$names] = round(log(rev(countData[[lineFits[[i]]$names]])[1]/rev(countData[[lineFits[[i]]$names]])[2]) * 100,2) #calculate the R value
     if (region == "Canada"){
       rValues[lineFits[[i]]$names] = as.numeric(rValues[lineFits[[i]]$names]) / 7
     }
@@ -131,11 +128,14 @@ plotCaseCountByDate2 <- function(countData, lineFits, population, region=NA, sav
   
   countData$type <- lineFits[[2]]$line$type
   
+  #melt the DF for plotting
   d <- countData %>% melt(id = c("Reported_Date", "n", "CaseCount", "report_type", "type")) 
   d$variable <- factor(d$variable , levels=levels(fct_relevel(sort(levels(d$variable)), "The Rest", after=0)))
   legendValues <- d %>% dplyr::select(variable) %>% mutate(variable = as.character(variable)) %>% unique() %>% 
     mutate(colorToUse = colors[variable]) %>% mutate(nameWithR = paste0(variable, "\n(r = ", round(as.numeric(rValues[variable]),0), "%)")) %>%
-    arrange(factor(variable, levels = levels(d$variable)))
+    arrange(factor(variable, levels = levels(d$variable))) #generate legend labels
+	
+	#add color and labels to the melted DF.
   legendColors <- legendValues$colorToUse
   legendLabels <- legendValues$nameWithR
   #duplicate the last row of the actual/accurate data so projections are show continiously.
@@ -147,6 +147,8 @@ plotCaseCountByDate2 <- function(countData, lineFits, population, region=NA, sav
   d$value <- (d$value*100000)/population
   d$CaseCount <- (d$CaseCount*100000)/population
   caseCountLabel <- paste0("Case Count\n(r = ", round(as.numeric(rValues["CaseCount"]),0), "%)")
+  
+  #plot
   p<- ggplot() +
     geom_area(data = d[d$type=="Actual" & d$report_type=="Accurate",], mapping = aes(x=Reported_Date, y=value, fill=variable),size = 0.5, alpha = 0.6, color="white", linetype="solid")+
     geom_area(data = d[d$type=="Projected" & d$report_type=="Accurate",], mapping = aes(x=Reported_Date, y=value, fill=variable), size = 0.5, alpha = 0.3,color="white",  linetype="dotted")+
