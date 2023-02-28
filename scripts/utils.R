e4b4dfd06bdffb3924962280ca4e43beaeaa4ee7
bfjia <bfjia@bfjia.net>
2023-02-28 11:52:07 -0800

    Show the top 3 individual variant selection over time plots only, hide the rest under a 'click here to open' tab

--- scripts/utils.R
+++ scripts/utils.R
@@ -42,3 +42,40 @@ getEmptyErrorPlotWithMessage <- function(message){
           text = element_text(size = 30)) 
   return (emptyErrorPlot)
 }
+
+#' This function generates an DT:datatable object to show tables on the HTML output
+#' @param dataframe dataframe to be dispalyed.
+DisplayHTMLTable <- function(dataframe){
+  DT::datatable(dataframe, 
+                extensions = 'Buttons', 
+                options = list(dom = 'Blfrtip',
+                              pageLength = 5, autoWidth = TRUE,
+                              #searchCols = list(NULL, list(search = 'regex')),
+                              search = list(regex = TRUE),
+                              buttons = list(
+                                list(
+                                  text = 'Export Displayed',
+                                  extend = 'csvHtml5',
+                                  filename = 'table',
+                                  exportOptions = list(
+                                    modifier = list(
+                                      page = 'current'
+                                    )
+                                  )
+                                ),
+                                list(
+                                  text = 'Export All',
+                                  extend = 'csvHtml5',
+                                  filename = 'table',
+                                  exportOptions = list(
+                                    modifier = list(
+                                      page = 'all'
+                                    )
+                                  )
+                                )
+                              ),
+                              lengthMenu = list(c(5,25,50,-1),
+                                                c(5,25,50,"All"))),
+                filter = 'top', rownames = F)
+}
+
