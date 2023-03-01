ed04f89e912cdc62d22bde8e5801f78c65f951d8
ArtPoon <artpoon@gmail.com>
2023-03-01 15:40:37 -0500

    passing data to d3, drawing scatterplot

--- js/rtt.js
+++ js/rtt.js
@@ -1,6 +1,8 @@
 console = d3.window(div.node()).console;
 console.log(data);
 
+var dateparser = d3.timeParse("%Y-%m-%d");
+
 var rttdiv = div.append("div")
                 .style("width", (width-100)+"px")
                 .style("height", (height-100)+"px")
@@ -15,7 +17,7 @@ var rttsvg = rttdiv.append("svg")
 // add margins
 var margin = {top: 10, right: 10, bottom: 10, left: 10},
     gwidth = width - 100 - margin.left - margin.right,
-    gheight = treeheight - margin.top - margin.bottom;
+    gheight = height - 100 - margin.top - margin.bottom;
   
 var rttg = rttsvg.append("g")
                  .attr("height", gheight+"px")
@@ -26,5 +28,19 @@ var rttg = rttsvg.append("g")
                        
 // set up plotting scales
 var ymax = d3.max(data, d => +d.div),
-    xScale = d3.scaleLinear().domain([0, xmax]).range([0, (gwidth-100) ]),
-    yScale = d3.scaleLinear().domain([0, ntips]).range([gheight, 40]),
+    ymin = d3.min(data, d => +d.div),
+    yScale = d3.scaleLinear().domain([ymin, ymax]).range([gheight, 40]),
+
+dates = data.map(d => dateparser(d.coldate));
+
+var xScale = d3.scaleLinear()
+               .domain(d3.extent(dates))
+               .range([0, (gwidth-100) ]);
+
+var rttplot = rttg.selectAll("circle")
+                  .data(data)
+                  .enter().append("circle")
+                  .attr("cx", function(d) { return xScale(dateparser(d.coldate)); })
+                  .attr("cy", function(d) { return yScale(d.div); })
+                  .attr("r", 3)
+                  .style("fill", "black");
