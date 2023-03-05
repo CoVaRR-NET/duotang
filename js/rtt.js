0dbdcfe6dcf941f26610fbc90039a974d6e7f35d
Art Poon <artpoon@gmail.com>
2023-03-04 23:30:12 -0500

    checkboxes coloured by PANGO group; bound event listener to checkboxes

--- js/rtt.js
+++ js/rtt.js
@@ -1,22 +1,31 @@
+// informed by http://emilydolson.github.io/D3-visualising-data/09-d3exit.html
+
 console = d3.window(div.node()).console;
 console.log(data);
 
 var tips = data.tips,
+    filtered_tips = tips, 
     palette = data.palette;
 
+// append entry for "other"
+palette["other"] = ["#777777"];
 
-var rtt_input = div.append("div")
+var ri_wide = 130,  // width of input column
+    rtt_input = div.append("div")
                    .attr("id", "rtt-input")
-                   .style("width", 100+"px")
+                   .style("width", ri_wide+"px")
                    .style("height", height+"px")
+                   .style("vertical-align", "top")
+                   .style("text-align", "right")
                    .style("display", "inline-block");
 
 var rttdiv = div.append("div")
-                .style("width", width-100+"px")
+                .style("width", width-ri_wide+"px")
                 .style("height", height+"px")
                 //.style("margin-top", "-30px")
                 .style("display", "inline-block");
 
+// render checkbox inputs labelled by PANGO group
 rtt_input.selectAll("input")
   .data(Object.entries(data.palette))
   .enter()
@@ -24,24 +33,46 @@ rtt_input.selectAll("input")
   .text(function(d) { return d[0]; })
   .append("input")
   .attr("type", "checkbox")
+  .attr("class", "rtt_cb")
+  .attr("value", function(d) { return d[0]; })
+  .style("accent-color", function(d) { return d[1][0]; })
   .attr("checked", true)
   .attr("id", function(d,i) { return i; })
   .attr("for", function(d,i) { return i; });
 
-// FIXME: this is not working yet
-d3.select("#rtt-input")
-  .selectAll("label")
+// render inputs on separate lines
+rtt_input.selectAll("label")
   .append("br");
 
+
+// bind event listener to checkboxes
+rtt_input.selectAll(".rtt_cb")
+  .on("change", function() {
+    if (this.checked) {
+      console.log("restore", this.value);
+      // restore group from original data
+      var pgroup = tips.filter(x => x.pango == this.value);
+      console.log(pgroup);
+      filtered_tips = filtered_tips.concat(pgroup);
+    } else {
+      console.log("remove", this.value);
+      // remove group from data
+      filtered_tips = filtered_tips.filter(x => x.pango != this.value);
+    }
+    console.log(filtered_tips);
+  })
+
+
+// prepare SVG for scatterplot
 var rttsvg = rttdiv.append("svg")
                    .attr("id", "rtt-svg")
-                   .attr("width", (width-100)+"px")
+                   .attr("width", (width-ri_wide)+"px")
                    .attr("height", (height)+"px");
 
 // add margins
 var margin = {top: 10, right: 10, bottom: 60, left: 60},
     // dimensions for the graph
-    gwidth = width - 100 - margin.left - margin.right,
+    gwidth = width - ri_wide - margin.left - margin.right,
     gheight = height - margin.top - margin.bottom;
   
 var rttg = rttsvg.append("g")
@@ -110,4 +141,5 @@ rttsvg.append("text")
       .attr("x", gwidth/2 + margin.left)
       .attr("y", gheight + margin.top + margin.bottom - 10)
       .text("Sampling date");
+
 
