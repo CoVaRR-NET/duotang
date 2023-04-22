b810b5558fe96416d1dcc591f6460dd62bfbda1e
bfjia <bfjia@bfjia.net>
2023-04-21 20:16:35 -0700

    add missing r script

--- scripts/plot_growing_lineages.R
+++ scripts/plot_growing_lineages.R
@@ -1,4 +1,6 @@
-plot_growing_lineage <- function(r, makeplot=TRUE){
+plot_growing_lineage <- function(r, makeplot=TRUE, coefficientTable=NA){
+  #r = paramselected[1:n]
+  #coefficientTable = coefficientTable
   d = data.frame(lineage = character(),
                  sel_coeff = numeric(),
                  low_CI = numeric(),
@@ -27,6 +29,16 @@ plot_growing_lineage <- function(r, makeplot=TRUE){
   
   d <- d%>%unique()
   
+  
+  if (unique(d$region) == "Canada" &  !is.na(coefficientTable)){
+    #generate the circle with borders if in more than 1 provinc for canada only plot
+    regionPresenceTable <- coefficientTable %>% dplyr::select(lineage, region) %>% filter (region != "Canada") %>% unique() %>% group_by(lineage) %>% summarise(NumRegions=n()) 
+    
+    d <- d %>% left_join(regionPresenceTable, by="lineage") %>% 
+      mutate (MultiRegion = ifelse(NumRegions > 1, 1, 0)) %>% dplyr::select(-NumRegions) %>% mutate(MultiRegion = replace_na(MultiRegion, 0))
+
+    }
+#iew(d)
   if(makeplot){
     bins=c(0,20,40,80,100,200,500,10000000)
     labelsstart <- head(gsub("(?<!^)(\\d{3})$", ",\\1", bins+1, perl=T),-1)
@@ -40,15 +52,26 @@ plot_growing_lineage <- function(r, makeplot=TRUE){
     d$lineage = factor(d$lineage, levels=d[order(d$sel_coeff),]$lineage)
     maxdate=max((meta %>%  filter(province %in% get.province.list(r[[1]]$region)))$sample_collection_date)
     title=paste("Most recent sequence date:",format(maxdate, "%B %d, %Y"))
-    p <- ggplot(d, aes(x=lineage, y=sel_coeff,colour=sequence_count))+
-    geom_point(size=5)+
+    
+    if (unique(d$region) == "Canada" & !is.na(coefficientTable) ){
+      p <- ggplot(d, aes(x=lineage, y=sel_coeff,colour=sequence_count, stroke=MultiRegion)) +
+        geom_point(size = 5, shape=21)
+    } else{
+      p <- ggplot(d, aes(x=lineage, y=sel_coeff,colour=sequence_count))+
+        geom_point(size = 5)
+    }
+    
+    p <- p +
     geom_pointrange( aes(ymin=low_CI, ymax=high_CI))+
     geom_hline(yintercept=10, linetype="dashed", color = "grey")+ #dash line at 10% per day to mark doubling in < week
-
     scale_y_continuous(breaks=seq(max((round(min(d$sel_coeff)/5))*5,-5),min(round(max(d$high_CI,11,na.rm = TRUE)/5)*5,15),5))+
     coord_flip()+ colScale+
-    ggtitle(title)+ labs(x="", y= paste("growth advantage (s% per day)\nrelative to ", individualSelectionPlotReference, " with 95% CI bars"))+theme_bw()
+    ggtitle(title)+ labs(x="", y= paste("growth advantage (s% per day)\nrelative to ", individualSelectionPlotReference, " with 95% CI bars"))+
+      labs(caption = "*Thick point border indicates a lineage have\na positive selection in multiple provinces") +
+      theme(plot.caption = element_text(color = "green", face = "italic")) +
+      theme_bw()
     #plot(p)
+    p
     return(p)
   }
   else{
