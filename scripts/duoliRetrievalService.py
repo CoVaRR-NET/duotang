759627914dff9410c5349799a691d905c0066ead
bfjia <bfjia@bfjia.net>
2023-05-01 16:33:55 -0700

    split the current situation text out of the rmd file into it's own md to make duoli integration easier

--- scripts/duoliRetrievalService.py
+++ scripts/duoliRetrievalService.py
@@ -23,22 +23,22 @@ if __name__ == "__main__":
     args = parser.parse_args()
     
     
-    if not (os.path.exists(".secret/duolioauthtoken") and os.path.exists(".secret/duoliapptoken")):
+    if not (os.path.exists("../.secret/duolioauthtoken") and os.path.exists("../.secret/duoliapptoken")):
         print ("This script reqires the oauth token and app token for duoli to be placed in .secret/duolioauthtoken and .secret/duoliapptoken")
         print ("Please check that they exist and the tokens are valid.")
         print ("Remeber to not push them into git.")
         sys.exit("token not found.")
 
-    with open (".secret/duolioauthtoken", 'r') as fh:
+    with open ("../.secret/duolioauthtoken", 'r') as fh:
         authToken = fh.readline().strip()
-    with open (".secret/duoliapptoken", 'r') as fh:
+    with open ("../.secret/duoliapptoken", 'r') as fh:
         appToken = fh.readline().strip()
 
     app = App(token=authToken)
     client = WebClient(token=authToken)
     
     channelID = args.channel
-    ts = args.ts
+    ts = "1682968319.934699" #args.ts
     rebuildCount = 0
     try:
         # Call the conversations.list method using the WebClient
@@ -48,6 +48,21 @@ if __name__ == "__main__":
                 #duoli was tagged in this message
                 if (msg["text"].find("/update") != -1):
                     rebuildCount = rebuildCount + 1
+                elif (msg["text"].find("/currentsituation") != -1):
+                    text = msg["text"].replace("<@U04R5U03V46> /currentsituation\n", "")
+                    #format the text a bit
+                    text = text.replace("**Current Situation:**", "").replace("*", "**").replace("•", "*").replace("_*","*").replace("*_","*").replace("\n","\n\n")
+                    text = text.split("\n")
+                    for i in range(len(text)):
+                        try:
+                            if text[i-1][:2] == "* " and text[i+1][:2] == "* ":
+                                del text[i]
+                                i = i - 1
+                        except:
+                            pass
+
+                    with open("currentsituation.md", "w+") as fh:
+                        fh.write("\n".join(text))
     except SlackApiError as e:
         print(f"Error: {e}")
     
