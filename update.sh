10e2feee0719f9110e54b6a96931627136949834
bfjia <bfjia@bfjia.net>
2023-05-05 23:13:31 -0700

    fix

--- update.sh
+++ update.sh
@@ -506,16 +506,16 @@ fi
 
 #echo "recordversion" > $checkPointFile
 #echo "$datestamp" > duotangCurVer
+cp data_needed/virusseq.$datestamp.fasta.xz data_needed/virusseq.fasta.xz
 
 echo "gitpush" > $checkPointFile
-
+	
 #gitpush:
 if [ "$GITPUSH" = "YES" ]; then 
 	#if [ "$BUILDMAIN" = "YES" ]; then 
 	#git remote prune origin
 	#git checkout -B UpdatePreview
 	#bash scripts/getPastDuotangVersions.sh
-	cp data_needed/virusseq.$datestamp.fasta.xz data_needed/virusseq.fasta.xz
 	git status
 	git add -f data_needed/*.nwk
 	git add -f data_needed/virusseq.metadata.csv.gz
