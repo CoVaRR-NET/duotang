1509a41b10494bc3f8c196f7d8e971ba272d425f
bfjia <bfjia@bfjia.net>
2023-10-27 09:54:01 -0700

    add a check to update monitor to halt updates if an update is in progress

---
+++ scripts/forceUpdate.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python scripts/UpdateStatusManager.py --action set --key Approved --value "True"
+python scripts/UpdateStatusManager.py --action set --key LastUpdated --value "2022-11-11"
