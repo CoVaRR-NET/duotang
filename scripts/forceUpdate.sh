33cced35aab556074609d3f534096e586103fbcf
bfjia <bfjia@bfjia.net>
2023-11-09 11:17:25 -0800

    forceupdate script will overwrite an in progress update

--- scripts/forceUpdate.sh
+++ scripts/forceUpdate.sh
@@ -2,3 +2,6 @@
 
 python scripts/UpdateStatusManager.py --action set --key Approved --value "True"
 python scripts/UpdateStatusManager.py --action set --key LastUpdated --value "2022-11-11"
+python scripts/UpdateStatusManager.py --action set --key LastVirusSeqReleaseID --value "FFFFF"
+python scripts/UpdateStatusManager.py --action set --key FailedCounter --value "0"
+rm -f checkpoint
