b602d17ac8fa3019d7174d43c347585fbccdd510
bfjia <bfjia@bfjia.net>
2024-09-12 07:59:01 -0700

    added dashed line to indicate failed CI, relabel the legends better

--- scripts/plot_growing_lineages.R
+++ scripts/plot_growing_lineages.R
@@ -1,8 +1,9 @@
 plot_growing_lineage <- function(r, makeplot=TRUE, coefficientTable="", mutantNamesToHighlight = ""){
   # r = paramselected[1:n]
   # coefficientTable = coefficientTable
-  # mutantNamesToHighlight = mutantNames
-  # 
+  # mutantNamesToHighlight = mutantToHighlight
+  # makeplot=T
+  # # 
   d = data.frame(lineage = character(),
                  sel_coeff = numeric(),
                  low_CI = numeric(),
@@ -55,17 +56,19 @@ plot_growing_lineage <- function(r, makeplot=TRUE, coefficientTable="", mutantNa
     mutantNamesToHighlight <- c(gsub("\\*", "", mutantNamesToHighlight), mutantNamesToHighlight)
     d <- d %>% mutate(Highlight = ifelse(lineage %in% mutantNamesToHighlight, 1, 0))
   }
-#iew(d)
+  
+  d <- d %>% mutate (lineType = ifelse(is.nan(low_CI), "solid", "dashed")) %>% mutate(low_CI = ifelse(is.nan(low_CI), sel_coeff * 0.9, low_CI)) %>% mutate(high_CI = ifelse(is.nan(high_CI), sel_coeff * 1.1, high_CI))
+  
   if(makeplot){
     bins=c(0,20,40,80,100,200,500,10000000)
     labelsstart <- head(gsub("(?<!^)(\\d{3})$", ",\\1", bins+1, perl=T),-1)
     labelsend <- tail(gsub("(?<!^)(\\d{3})$", ",\\1", bins, perl=T),-1)
     labelsend[[length(labelsend)]]=""
     rangelabels <- paste(labelsstart, labelsend, sep="-")
-    d$sequence_count=as.factor(cut(d$size,bins,rangelabels,left = FALSE))
-    couleur=rev(hcl.colors(length(levels(d$sequence_count)), "Red-Blue"))
-    names(couleur)=levels(d$sequence_count)
-    colScale=scale_colour_manual(name="sequence_count",values=couleur)
+    d$`Number of Sequences`=as.factor(cut(d$size,bins,rangelabels,left = FALSE))
+    couleur=rev(hcl.colors(length(levels(d$`Number of Sequences`)), "Red-Blue"))
+    names(couleur)=levels(d$`Number of Sequences`)
+    colScale=scale_colour_manual(name="Number of Sequences",values=couleur)
     d$lineage = factor(d$lineage, levels=d[order(d$sel_coeff),]$lineage)
     
     maxdate=max((meta %>%  filter(province %in% get.province.list(r[[1]]$region)))$sample_collection_date, na.rm=T)
@@ -74,17 +77,17 @@ plot_growing_lineage <- function(r, makeplot=TRUE, coefficientTable="", mutantNa
     if (class(coefficientTable) == "data.frame" ){
       if (unique(d$region) == "Canada")
       {
-        p <- ggplot(d, aes(x=lineage, y=sel_coeff,colour=sequence_count, stroke=MultiRegion)) +
+        p <- ggplot(d, aes(x=lineage, y=sel_coeff,colour=`Number of Sequences`, stroke=MultiRegion)) +
           geom_point(size = 5, shape=21)
       } else{
-      p <- ggplot(d, aes(x=lineage, y=sel_coeff,colour=sequence_count))+
+      p <- ggplot(d, aes(x=lineage, y=sel_coeff,colour=`Number of Sequences`))+
         geom_point(size = 5)
       }
     }
     axisMax <- ifelse(is.finite(max(round((d$high_CI + 2.5)/ 5.0) * 5.0)), max(round((d$high_CI + 2.5)/ 5.0) * 5.0),max(max(round((d$sel_coeff + 2.5)/ 5.0) * 5.0)) )
     #annotate lines and CIs
     p <- p +
-    geom_pointrange( aes(ymin=low_CI, ymax=high_CI))+
+    geom_pointrange( aes(ymin=low_CI, ymax=high_CI, linetype = lineType), show.legend = F)+
     geom_hline(yintercept=10, linetype="dashed", color = "grey")+ #dash line at 10% per day to mark doubling in < week
     scale_y_continuous(breaks=seq(0, axisMax,5))+
     coord_flip()+ colScale
@@ -100,9 +103,9 @@ plot_growing_lineage <- function(r, makeplot=TRUE, coefficientTable="", mutantNa
     p<- p + theme_bw()+
     theme(plot.caption.position = "plot", plot.caption = element_text(hjust=0)) +
     ggtitle(title)+ labs(x="", y= paste("growth advantage (s% per day)\nrelative to ", individualSelectionPlotReference, " with 95% CI bars"))+
-      labs(caption = "*Circled dots indicate lineages with a positive selection coefficient in multiple provinces\n**Gray shading indicates the most prevalent strain in the last two weeks") 
+      labs(caption = "*Circled dots indicate lineages with a positive selection coefficient in multiple provinces\n*Gray shading indicates the most prevalent strain in the last two weeks\n*Dashed line indicates the inability to estimate CI, likely do to low case counts.") 
     #plot(p)
-    #p
+    p
     return(p)
   }
   else{
